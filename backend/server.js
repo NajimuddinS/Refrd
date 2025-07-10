a6aa689da7bf9719569a3ff546191319cf9d0918
Najimuddin Shaikh <sherlockshaikh@gmail.com>
2025-07-10 20:10:59 +0530

    files added

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,14 @@ const MongoDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const candidateRoutes = require('./routes/candidateRoutes');
 
-app.use(cors({
-  origin: 'http://localhost:5173', // Allow only your frontend origin
-  credentials: true // If you're using cookies/sessions
-}));
+
 
 const app = express();
+
+app.use(cors({
+  origin: true, // Allow only your frontend origin
+  credentials: true // If you're using cookies/sessions
+}));
 app.use(express.json());
 app.use("/api/users", userRoutes);
 app.use('/api/candidates', candidateRoutes);
