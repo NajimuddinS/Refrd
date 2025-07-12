e648ddb72ac435d1a2419d48b9a0f1eb858dc982
Najimuddin Shaikh <sherlockshaikh@gmail.com>
2025-07-12 14:36:02 +0530

    cors fixed

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,26 @@
 const express = require("express");
 require("dotenv").config();
-const cors = require('cors');
+const cors = require("cors");
 const MongoDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
-const candidateRoutes = require('./routes/candidateRoutes');
+const candidateRoutes = require("./routes/candidateRoutes");
 
 const app = express();
 
-app.use(cors({
-  origin: ['https://refrd.vercel.app'],
+const corsOptions = {
+  origin: ["https://refrd.vercel.app"],
   credentials: true,
-}));
+};
 
-app.options('*', cors()); 
+app.use(cors(corsOptions)); 
 
 app.use(express.json());
+
 app.use("/api/users", userRoutes);
-app.use('/api/candidates', candidateRoutes);
+app.use("/api/candidates", candidateRoutes);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   MongoDB();
-  console.log(`running on ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
